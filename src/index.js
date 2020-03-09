325337c6e80f4e3e0fcde5425b56fc7224de340d
Emerson O da Hora <emersondahora@gmail.com>
2020-03-08 21:50:24 -0300

    Iniciando projeto

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import './config/ReactotronConfig';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Inicio do Projeto</Text>
+    </View>
+  );
+}
